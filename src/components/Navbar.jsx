83373c3df2d5b2152adccbc0b8c90aa36cff18b9
Priyanshu garg <gpriyanshu585@gmail.com>
2025-09-03 22:07:03 +0530

    added icons and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { GraduationCap, Briefcase, Dog } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-aqua-600 p-4 flex justify-center space-x-6 shadow-md">
-      <Link
+    <nav className="bg-gray-400 p-4 flex justify-center space-x-6 shadow-md">
+      <NavLink
         to="/student"
-        className={({ isActive }) => 
-          `px-4 py-2 rounded-md font-semibold text-white transition ${
-            isActive ? "bg-indigo-800" : "hover:bg-indigo-500"
-          }`
+        className={({ isActive }) =>
+          isActive
+            ? "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold bg-black"
+            : "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
         }
       >
-        Student Form
-      </Link>
-      <Link
+        <GraduationCap size={20} />
+        <span>Student Form</span>
+      </NavLink>
+
+      <NavLink
         to="/employee"
         className={({ isActive }) =>
-          `px-4 py-2 rounded-md font-semibold text-white transition ${
-            isActive ? "bg-indigo-800" : "hover:bg-indigo-500"
-          }`
+          isActive
+            ? "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold bg-black"
+            : "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
         }
       >
-        Employee Form
-      </Link>
+        <Briefcase size={20} />
+        <span>Employee Form</span>
+      </NavLink>
 
-      <Link
+      <NavLink
         to="/dog-adoption"
         className={({ isActive }) =>
-          `px-4 py-2 rounded-md font-semibold text-white transition ${
-            isActive ? "bg-indigo-800" : "hover:bg-indigo-500"
-          }`
+          isActive
+            ? "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold bg-black"
+            : "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
         }
       >
-        Dog Adoption Form
-      </Link>
+        <Dog size={20} />
+        <span>Dog Adoption Form</span>
+      </NavLink>
     </nav>
   );
 };
 
 export default Navbar;
+
