142cf75c4f8ee424df83370df23b657acc26e585
Priyanshu garg <gpriyanshu585@gmail.com>
2025-09-04 15:00:21 +0530

    errors resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,35 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { GraduationCap, Briefcase, Dog } from "lucide-react";
+import { Link } from "react-router-dom";
+import { GraduationCap, Briefcase, Dog, Cat } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-400 p-4 flex justify-center space-x-6 shadow-md">
-      <NavLink
+      <Link
         to="/student"
-        className={({ isActive }) =>
-          isActive
-            ? "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold bg-black"
-            : "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
-        }
+        className="flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
       >
         <GraduationCap size={20} />
         <span>Student Form</span>
-      </NavLink>
+      </Link>
 
-      <NavLink
+      <Link
         to="/employee"
-        className={({ isActive }) =>
-          isActive
-            ? "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold bg-black"
-            : "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
-        }
+        className="flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
       >
         <Briefcase size={20} />
         <span>Employee Form</span>
-      </NavLink>
+      </Link>
 
-      <NavLink
+      <Link
         to="/dog-adoption"
-        className={({ isActive }) =>
-          isActive
-            ? "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold bg-black"
-            : "flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
-        }
+        className="flex items-center gap-2 px-4 py-2 rounded-md text-white font-bold hover:bg-black"
       >
         <Dog size={20} />
         <span>Dog Adoption Form</span>
-      </NavLink>
+      </Link>
     </nav>
   );
 };
 
 export default Navbar;
-
